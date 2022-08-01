d5f5cd1633b74475ac55a61251ee243930dca969
Miguel Ferreira <Miguel.FA.Ferreira@ctw.bmwgroup.com>
2022-08-01 11:57:50 +0100

    update board based on state and find next player turn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,58 @@
 import type { NextPage } from 'next'
+import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 import HeadComponent from '../components/HeadComponent/HeadComponent';
 import styles from '../styles/Home.module.scss';
+import {BOARD_CELLS, PLAYER_1, PLAYER_2} from '../utils/globals';
 
-const BOARD_CELLS = 9;
 
 const Home: NextPage = () => {
+  const boardInitialState: Array<String> = ['','','','','','','','',''];
+  const [board, setBoard] = useState(boardInitialState);
+  const [currentPlayer, seCurrentPlayer] = useState(PLAYER_1);
 
-  const handleCellClick = (index: any) => {
-    console.log(index);
+  const updateBoard = (index: number) => {
+    let updatedBoard = [...board];
+    updatedBoard[index] = currentPlayer;
+    setBoard(updatedBoard);
   };
 
- const renderBoard = () => 
-    <div className={styles.boardContainer}>
-      {[...Array(BOARD_CELLS)].map((item, index) => 
-        <button 
-          key={uuidv4()}
-          className={styles[`cell-${index}`]}
-          onClick={()=> handleCellClick(index)}>X</button>)
-      }
-    </div>;
+  const updateNextPlayerTurn = () => currentPlayer === PLAYER_1 ? seCurrentPlayer(PLAYER_2) : seCurrentPlayer(PLAYER_1);
+
+  const isCellEmpty = (index: number) => !board[index];
+
+  const renderBoard = () => 
+      <div className={styles.boardContainer}>
+        {[...Array(BOARD_CELLS)].map((item, index : number) => 
+          <button 
+            key={uuidv4()}
+            className={styles[`cell-${index}`]}
+            onClick={()=> handleCellClick(index)}>{board[index]}</button>)
+        }
+      </div>;
+
+  // Handlers    
+
+  const handleCellClick = (index: number) => {
+    if(isCellEmpty(index)){
+      updateBoard(index);
+      updateNextPlayerTurn();
+    }
+  };
+
+  const handleRestartGameClick = () => {
+    setBoard(boardInitialState);
+    seCurrentPlayer(PLAYER_1);
+  };
 
   return (
     <div className={styles.container}>
       <HeadComponent />
       <main className={styles.main}>
-        <div className={styles.start}>Start playing | Its turn of X</div> 
+        <div className={styles.start}>Its turn of {currentPlayer}</div> 
         <div>{renderBoard()}</div>
-        <div className={styles.restart}>Restart Game</div>
+        <div className={styles.restart} onClick={handleRestartGameClick}>Restart Game</div>
       </main>
     </div>
   )
---
+++ utils/globals.ts
@@ -0,0 +1,3 @@
+export const BOARD_CELLS: number = 9;
+export const PLAYER_1: string = 'X';
+export const PLAYER_2: string = 'O';
